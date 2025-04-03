c8ce72d5883fd5dc0e37efa94ca202dbceaff76b
Sanket Gohel <sanket@Sankets-MacBook-Pro.local>
2025-04-02 22:20:49 -0400

    active users

---
+++ Active_Users.sql
@@ -0,0 +1,84 @@
+/*Table: Accounts
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+This table contains the account id and the user name of each account.
+ 
+
+Table: Logins
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| login_date    | date    |
++---------------+---------+
+This table may contain duplicate rows.
+This table contains the account id of the user who logged in and the login date. A user may log in multiple times in the day.
+ 
+
+Active users are those who logged in to their accounts for five or more consecutive days.
+
+Write a solution to find the id and the name of active users.
+
+Return the result table ordered by id.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Accounts table:
++----+----------+
+| id | name     |
++----+----------+
+| 1  | Winston  |
+| 7  | Jonathan |
++----+----------+
+Logins table:
++----+------------+
+| id | login_date |
++----+------------+
+| 7  | 2020-05-30 |
+| 1  | 2020-05-30 |
+| 7  | 2020-05-31 |
+| 7  | 2020-06-01 |
+| 7  | 2020-06-02 |
+| 7  | 2020-06-02 |
+| 7  | 2020-06-03 |
+| 1  | 2020-06-07 |
+| 7  | 2020-06-10 |
++----+------------+
+Output: 
++----+----------+
+| id | name     |
++----+----------+
+| 7  | Jonathan |
++----+----------+
+Explanation: 
+User Winston with id = 1 logged in 2 times only in 2 different days, so, Winston is not an active user.
+User Jonathan with id = 7 logged in 7 times in 6 different days, five of them were consecutive days, so, Jonathan is an active user.*/
+
+
+
+WITH ConsecutiveLogins AS (
+    SELECT id, login_date,
+        login_date  - (ROW_NUMBER() OVER (PARTITION BY id ORDER BY login_date) || 'DAY') ::INTERVAL AS shifted_dates
+    FROM (SELECT DISTINCT id, login_date FROM logins) AS distinct_logins
+)
+SELECT DISTINCT a.id, a.name
+FROM (
+    SELECT id, shifted_dates, COUNT(*) AS consecutive_days
+    FROM ConsecutiveLogins
+    GROUP BY id, shifted_dates
+    HAVING COUNT(*) >= 5
+) AS active_users
+JOIN Accounts a ON active_users.id = a.id
+ORDER BY a.id;
